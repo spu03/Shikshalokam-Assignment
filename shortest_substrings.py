082a2cd5fae4d623c409579ba8443641a7391a8d
spu03 <spurthikalakeri@gmail.com>
2024-01-09 13:25:27 +0530

    Add files via upload

---
+++ shortest_substrings.py
@@ -0,0 +1,20 @@
+def shortest_substrings(s, x):
+    substrings = []
+    for i in range(len(s)):
+        for j in range(i + x, len(s) + 1):
+            substring = s[i:j]
+            if len(substring) == x and substring[0] == substring[-1]:
+                substrings.append(substring)
+    return substrings
+
+def print_shortest_substrings(s, x):
+    substrings = shortest_substrings(s, x)
+    if not substrings:
+        print("Answer: not-found")
+    else:
+        print("Answer:", " ".join(substrings))
+
+s = input("string: ")
+x = int(input("x: "))
+
+print_shortest_substrings(s,x)
